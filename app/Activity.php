c41b5b83154704fbeb2f01a6cc1fca01373c41b2
Janine Hempy <jhempy@gmail.com>
2018-07-10 14:23:24 -0400

    WIP, working on activities table for admins

--- app/Activity.php
+++ app/Activity.php
@@ -3,10 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Activity extends Model
 {
     public function groups() {
     	return $this->hasMany('App\Group');
+    }
+
+    public function participants() {
+
+    	$results = DB::select('SELECT count(*) FROM group_user
+WHERE group_id IN (SELECT id FROM groups WHERE activity_id = ?)', [$this->id]);
+
+    	return $results[0]->count;
+
     }
 }
--- resources/views/activities/index.blade.php
+++ resources/views/activities/index.blade.php
@@ -6,6 +6,29 @@
 
 @section('content')
 
-Hello from activities page!
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th># Groups</th>
+				<th># Participants</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+
+			@foreach ($activities as $activity)
+
+				<tr>
+					<td>{{ $activity->name }}</td>
+					<td>{{ $activity->groups()->count() }}</td>
+					<td>{{ $activity->participants() }}</td>
+					<td>Edit | Delete</td>
+				</tr>
+
+			@endforeach
+
+		</tbody>
+	</table>
 
 @endsection
