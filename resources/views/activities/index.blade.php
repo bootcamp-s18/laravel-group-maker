8291940708267b9b6014258e469aa1dd182e8aa5
Janine Hempy <jhempy@gmail.com>
2018-07-11 14:16:31 -0400

    Toggles input form for new activity

--- resources/views/activities/index.blade.php
+++ resources/views/activities/index.blade.php
@@ -6,20 +6,36 @@
 
 @section('content')
 
-	<a href="#">New Activity</a>
+	<script>
+		function toggleNewActivityForm() {
+		    var x = document.getElementById("newActivityForm");
+		    var t = document.getElementById("toggleText");
+		    if (x.style.display === "none") {
+		        x.style.display = "block";
+		        t.innerHTML = "Hide New Activity Form";
+		    } else {
+		        x.style.display = "none";
+		        t.innerHTML = "Show New Activity Form";
+		    }
+		}
+	</script>
 
-	<form class="pb-4" method="post" action="/activities">
-		@csrf
-		<div class="form-group">
-			<label for="name">Activity Name</label>
-			<input type="text" class="form-control" name="activityName" id="name">
-		</div>
-		<div class="form-group">
-			<label for="description">Activity Description</label>
-			<textarea class="form-control" id="description" name="activityDescription" rows="3"></textarea>
-		</div>
-		<button class="btn btn-primary" type="submit">Create</button>
-	</form>
+	<p><a id="toggleText" href="#" onclick="toggleNewActivityForm()">Show New Activity Form</a></p>
+
+	<div id="newActivityForm" style="display: none;" class="p-3 m-3">
+		<form class="pb-4" method="post" action="/activities">
+			@csrf
+			<div class="form-group">
+				<label for="name">Activity Name</label>
+				<input type="text" class="form-control" name="activityName" id="name">
+			</div>
+			<div class="form-group">
+				<label for="description">Activity Description</label>
+				<textarea class="form-control" id="description" name="activityDescription" rows="3"></textarea>
+			</div>
+			<button class="btn btn-primary" type="submit">Create</button>
+		</form>
+	</div>
 
 	<table class="table">
 		<thead>
